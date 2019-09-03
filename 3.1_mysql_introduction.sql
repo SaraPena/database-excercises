e1e450573d334151257550a90257e4cbaa2c6c1e
SaraPena <pena.e.sara@gmail.com>
2019-09-03 14:19:16 -0500

    MySQL Query Intro

---
+++ 3.1_mysql_introduction.sql
@@ -0,0 +1,13 @@
+show databases;
+
+-- mysql.user means use the mysql database and read from the user table
+select * from mysql.user;
+
+-- select the user and host columns from the user table
+select user, host from mysql.user;
+
+-- select all columns from the help_topic table on the mysql database
+SELECT * FROM mysql.help_topic;
+
+-- select only only the help_topic_id, help_category_id 
+SELECT help_topic_id, help_category_id, url FROM mysql.help_topic;
