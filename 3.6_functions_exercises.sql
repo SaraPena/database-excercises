4961f390c31bdf9e393ab1006d556f6036283dac
SaraPena <pena.e.sara@gmail.com>
2020-02-03 14:41:15 -0600

    Update exercise files

--- 3.6_functions_exercises.sql
+++ 3.6_functions_exercises.sql
@@ -1,29 +1,17 @@
- -- 1. Copy the order by exercise and save it as 3.6_functions_exercises.sql
- 
- -- 2. Update your queries for employees who names start and end with 'E'. Use concat() to combine their first and last name together as a single column named full_name.
- -- 3. Convert the names produced in your last query to all uppercase.
- 
- USE employees;
- Select UPPER(Concat(first_name,' ',last_name)) as full_name 
- 	from employees 
- 		where Concat(first_name,' ',last_name) like 'E%E';
- 
--- 4. For your query of employees born on Christmas and hired in the 90s, use datediff() to find out how many days they have been working at the company. (HINT: you will also need to use NOW() or CURDATE()).
-
-SELECT datediff(NOW(),hire_date) as days_worked, hire_date FROM employees 
-	WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') 
-		AND (birth_date LIKE '%12-25%')
-		ORDER BY days_worked DESC;
--- 5. Find the smallest and largest salary from the salaries table. 
-Select min(salary) as "smallest salary", max(salary) as "Maximum salary" from salaries;
-
--- 6. Use your knowledge of built in SQL functions to generate a username for all the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 character of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born.
-Select lower(CONCAT(
+USE employees; 
+SELECT * FROM employees;
+SELECT  gender, count(gender) 
+	FROM employees 
+	WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+	GROUP BY gender;
+	
+Select COUNT(DISTINCT (lower(CONCAT(
 	SUBSTR(first_name,1,1), 
 	SUBSTR(last_name,1,4), "_", 
 	SUBSTR(birth_date,6,2), 
-	SUBSTR(birth_date,3,2))) 
-	as username from employees;
-
- 
+	SUBSTR(birth_date,3,2)))))
+	as username, count(*) from employees
+	;
+	
+	
 
