be854c275af4365dec60b7366d759af17688bdf1
SaraPena <pena.e.sara@gmail.com>
2020-02-03 15:41:00 -0600

    Update with exercise instructions

--- REVIEW/3.2_databases.sql
+++ REVIEW/3.2_databases.sql
@@ -1,11 +1,19 @@
+-- Exercises
+-- Connect to the database server
+/* 1. List all the databases */
 SHOW Databases;
 
+/* 2. Switch to a database using the USE statement */
 USE chipotle;
 
+/* 3. Show the currently selected database */
 SELECT database();
 
+/* 4. Find out the query used to create the database */
 SHOW CREATE DATABASE chipotle;
 
+/* 5. Switch to a different database */
 USE titanic_db;
 
+/* 6. Show the currently selected database */
 SELECT database();
