23466c42d4a68b211f6a3d8a9f331a8367b67a56
SaraPena <pena.e.sara@gmail.com>
2020-02-04 17:37:38 -0600

    Create 3.4_select_exercises.sql

--- REVIEW/3.4_select_exercises.sql
+++ REVIEW/3.4_select_exercises.sql
@@ -1,11 +1,62 @@
-/* For example, to select the fruits and their quantity in our fruits database */
-SHOW databases;
-USE fruits_db;
+/*Exercises
+1. Create a new file named 3.4_select_exercises.sql. Do you work for these exercises in that file.
 
-SELECT name, quantity
-FROM fruits;
+2. Use the `albums_db` database */
 
-/* If we want to retrieve all of the available columns for a database table, we can use the wildcard *. */
+/* SHOW databases; */
 
-SELECT * FROM fruits;
+USE albums_db;
 
+/* 3. Explore the structure of the `albums` table */
+
+DESCRIBE albums;
+
+/* 4. Write queries to find the following information.
+ - The name of all albums by Pink Floyd*/
+ 
+SELECT 'Albums by Pink Floyd' as INFO;
+ 
+SELECT name
+From albums
+WHERE artist = 'Pink Floyd';
+
+/* The year Sgt. Pepper's Lonely Hearts Club Band was released*/
+
+SELECT 'The year Sgt. Pepper\'s Lonely Hearts Club Band was released' as INFO;
+
+SELECT release_date
+FROM albums
+WHERE name = "Sgt. Pepper's Lonely Hearts Club Band";
+
+/*The genre of the album Nevermind*/
+
+SELECT 'Genre of the album Nevermind' as INFO;
+
+SELECT genre
+FROM albums
+WHERE name = 'Nevermind';
+
+/* Which albums were relased in the 90's*/
+
+SELECT 'Which albums were released in the 90\'s' as INFO;
+
+SELECT name
+From albums
+WHERE release_date BETWEEN 1990 and 1999;
+
+/* Which albums had less than 20 million certified sales*/
+
+SELECT 'Which albums had less than 20 million certified sales' as INFO;
+
+SELECT name
+FROM albums
+WHERE sales < 20;
+
+/*All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+ANSWER - using the `=` operator only gives you exact matches to Rock. */
+
+SELECT 'All the albums with a genre \'Rock\'' as INFO;
+
+SELECT name, genre
+FROM albums
+WHERE genre = 'ROCK';
