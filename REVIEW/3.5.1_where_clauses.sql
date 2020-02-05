5c8ac7c5f37665fe8619e77a943c1bf1daf96205
SaraPena <pena.e.sara@gmail.com>
2020-02-05 11:58:12 -0600

    Create 3.5.1_where_clauses.sql

---
+++ REVIEW/3.5.1_where_clauses.sql
@@ -0,0 +1,21 @@
+USE employees;
+
+SELECT * 
+FROM employees 
+WHERE hire_date = '1985-01-01';
+
+SELECT first_name
+FROM employees
+WHERE first_name LIKE '%sus%';
+
+SELECT DISTINCT first_name
+FROM employees
+WHERE first_name LIKE '%sus%';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE emp_no BETWEEN 10026 and 10082;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name IN ('Herber', 'Dredge', 'Lipner', 'Black');
